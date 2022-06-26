c2e161a56d67be4fd64fcd3edbaea2e3a8f9f456
Patrick Chan <chanpatorikku@gmail.com>
2022-06-26 23:55:22 +0900

    add mvcmovie

---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // 
+        // GET: /HelloWorld/
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+        // 
+        public IActionResult Welcome(string name = "efw", int numTimes = 3)
+        {
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
+        }
+
+
+        /*
+        public string Welcome(string name ="wefwe", int numTimes = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+        }
+        */
+    }
+}
